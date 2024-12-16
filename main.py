1ac1a721e3ea6650bc98b9f6b908e2bf4caa22e0
Lukasz Gren <18073043+Boldeor@users.noreply.github.com>
2024-12-16 18:18:03 +0100

    Draw ship

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     
 
 
